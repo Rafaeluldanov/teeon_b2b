746aa2d4e443f420c5176d3ccf938946da7be43d
ivan <shapoclode@gmail.com>
2026-06-16 09:35:58 +0000

    auto: update CatalogSection.tsx

--- components/CatalogSection/CatalogSection.tsx
+++ components/CatalogSection/CatalogSection.tsx
@@ -72,6 +72,8 @@ export default async function CatalogSection() {
                         src={src}
                         alt=""
                         className={styles.mediaCollageImg}
+                        loading="lazy"
+                        decoding="async"
                       />
                     ))}
                   </div>
